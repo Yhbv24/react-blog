2c2af1cb89587ac2bfb9ed0d0f7f58a4d5d42401
Ash Laidlaw <ash.laidlaw@vacasa.com>
2018-11-05 15:57:54 -0800

    Add Webpack build settings

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+/**
+ * Webpack configuration
+ */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/scripts'),
+    filename: 'bundle.js'
+  }
+};
